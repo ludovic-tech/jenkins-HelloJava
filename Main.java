83d4b0546ae236f3fd4bbdee74901953e68d96eb
ludovic.tech <it.quenechdu@gmail.com>
2021-03-23 12:45:16 +0100

    WebHook Github

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nouvelle Compilation auto via triggers Jenkins !");
+        System.out.println("Nouvelle Compilation auto via WebHook Githubb !");
     }
 }
