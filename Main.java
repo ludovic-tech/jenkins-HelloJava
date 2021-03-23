2155e1845925d5318d3957432dd633d950639a27
ludovic.tech <it.quenechdu@gmail.com>
2021-03-23 14:08:03 +0100

    URL + PUSH avec TAG

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nouvelle Compilation auto via WebHook Githubb avec validation jenkins URL + PUSH avec TAG!");
+        System.out.println("Nouvelle Compilation auto via WebHook Githubb avec validation jenkins !");
     }
 }
